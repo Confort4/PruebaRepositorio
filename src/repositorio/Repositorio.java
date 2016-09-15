39d090d2e82baffdd03df7915ee70f6d778948bc
2dama <2dama@PC221017.informatica.edu>
2016-09-15 15:59:55 +0200

    Añadido dos print.

--- src/repositorio/Repositorio.java
+++ src/repositorio/Repositorio.java
@@ -2,5 +2,7 @@ package repositorio;
 public class Repositorio {
     public static void main(String[] args) {
         System.out.print("Proyecto Repositorio");
+        System.out.print("Proyecto Repositorio 2");
+        System.out.print("Proyecto Repositorio 3");
     }
 }
